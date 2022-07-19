9fc76a13dfeec09b08af13cf6920c3d4970f74df
vigneshwaripandi <90615027+vigneshwaripandi@users.noreply.github.com>
2022-07-19 15:59:13 +0100

    ECC-1141 reverted controller changes for Standalone Eori already exists

--- app/uk/gov/hmrc/eoricommoncomponent/frontend/controllers/YouAlreadyHaveEoriController.scala
+++ app/uk/gov/hmrc/eoricommoncomponent/frontend/controllers/YouAlreadyHaveEoriController.scala
@@ -22,7 +22,7 @@ import play.api.mvc._
 import uk.gov.hmrc.eoricommoncomponent.frontend.controllers.auth.AuthAction
 import uk.gov.hmrc.eoricommoncomponent.frontend.domain.LoggedInUserWithEnrolments
 import uk.gov.hmrc.eoricommoncomponent.frontend.models.Service
-import uk.gov.hmrc.eoricommoncomponent.frontend.views.html.{standalone_already_have_eori, you_already_have_eori}
+import uk.gov.hmrc.eoricommoncomponent.frontend.views.html.you_already_have_eori
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
 
 import scala.concurrent.Future
@@ -30,7 +30,6 @@ import scala.concurrent.Future
 class YouAlreadyHaveEoriController @Inject() (
   authAction: AuthAction,
   eoriExistsView: you_already_have_eori,
-  standAloneView: standalone_already_have_eori,
   mcc: MessagesControllerComponents
 ) extends FrontendController(mcc) with I18nSupport {
 
