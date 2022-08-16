eec9b5fa452e8a1701b025c787d2e271a8832788
oliwiaKot <48218839+oliwiaKot@users.noreply.github.com>
2022-08-16 14:17:38 +0100

    ECC-1239| formatted spec file

--- test/unit/controllers/DoYouHaveAUtrNumberControllerSpec.scala
+++ test/unit/controllers/DoYouHaveAUtrNumberControllerSpec.scala
@@ -189,7 +189,8 @@ class DoYouHaveAUtrNumberControllerSpec
           "Do you have a Self Assessment Unique Taxpayer Reference (UTR) number issued in the UK?"
         )
         page.h1 shouldBe "Do you have a Self Assessment Unique Taxpayer Reference (UTR) number issued in the UK?"
-        page.getElementsText("//*[@id='have-utr-hint']") shouldBe "This is 10 numbers, for example 1234567890, which may be followed by a K. It will be on tax returns and other letters about Income Tax. It may be called reference, UTR or official use. You can find a lost UTR number ."
+        page.getElementsText("//*[@id='have-utr-hint']") shouldBe
+          "This is 10 numbers, for example 1234567890, which may be followed by a K. It will be on tax returns and other letters about Income Tax. It may be called reference, UTR or official use. You can find a lost UTR number ."
       }
     }
     "contain a proper content for individuals" in {
@@ -202,7 +203,8 @@ class DoYouHaveAUtrNumberControllerSpec
           "Do you have a Self Assessment Unique Taxpayer Reference (UTR) number issued in the UK?"
         )
         page.h1 shouldBe "Do you have a Self Assessment Unique Taxpayer Reference (UTR) number issued in the UK?"
-        page.getElementsText("//*[@id='have-utr-hint']") shouldBe "This is 10 numbers, for example 1234567890, which may be followed by a K. It will be on tax returns and other letters about Income Tax. It may be called reference, UTR or official use. You can find a lost UTR number ."
+        page.getElementsText("//*[@id='have-utr-hint']") shouldBe
+          "This is 10 numbers, for example 1234567890, which may be followed by a K. It will be on tax returns and other letters about Income Tax. It may be called reference, UTR or official use. You can find a lost UTR number ."
       }
     }
   }
