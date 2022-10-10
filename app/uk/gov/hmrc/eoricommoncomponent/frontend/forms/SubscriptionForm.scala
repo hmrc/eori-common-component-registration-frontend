8e8949c86df6263669349f6ceb668c9bdd63963e
Jake Goodwin <12535866+JakeGoodwin@users.noreply.github.com>
2022-10-10 14:30:19 +0100

    Formatting

--- app/uk/gov/hmrc/eoricommoncomponent/frontend/forms/SubscriptionForm.scala
+++ app/uk/gov/hmrc/eoricommoncomponent/frontend/forms/SubscriptionForm.scala
@@ -53,11 +53,14 @@ object SubscriptionForm {
 
   private def validSicCode: Constraint[String] =
     Constraint("constraints.sic")({
-      case s if s.filterNot(_.isWhitespace).isEmpty       => Invalid(ValidationError("cds.subscription.sic.error.empty"))
-      case s if !s.filterNot(_.isWhitespace).matches("[0-9]*") => Invalid(ValidationError("cds.subscription.sic.error.wrong-format"))
-      case s if s.filterNot(_.isWhitespace).length < 4         => Invalid(ValidationError("cds.subscription.sic.error.too-short"))
-      case s if s.filterNot(_.isWhitespace).length > 5         => Invalid(ValidationError("cds.subscription.sic.error.too-long"))
-      case _                         => Valid
+      case s if s.filterNot(_.isWhitespace).isEmpty => Invalid(ValidationError("cds.subscription.sic.error.empty"))
+      case s if !s.filterNot(_.isWhitespace).matches("[0-9]*") =>
+        Invalid(ValidationError("cds.subscription.sic.error.wrong-format"))
+      case s if s.filterNot(_.isWhitespace).length < 4 =>
+        Invalid(ValidationError("cds.subscription.sic.error.too-short"))
+      case s if s.filterNot(_.isWhitespace).length > 5 =>
+        Invalid(ValidationError("cds.subscription.sic.error.too-long"))
+      case _ => Valid
     })
 
   def validEoriWithOrWithoutGB: Constraint[String] =
