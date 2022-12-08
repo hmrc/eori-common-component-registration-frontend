cd8ad7ef4c12ecdc42083a49b0bc050b90ea76bd
Jake Goodwin <12535866+JakeGoodwin@users.noreply.github.com>
2022-12-08 13:17:40 +0000

    ECC-1247

--- test/unit/views/YouCantUseServiceSpec.scala
+++ test/unit/views/YouCantUseServiceSpec.scala
@@ -31,78 +31,18 @@ class YouCantUseServiceSpec extends ViewSpec {
   private val sub02View             = instanceOf[sub02_request_not_processed]
 
   "You cannot use this service page for users of type standard org" should {
-
-    "display correct title" in {
-      standardOrgDoc.title must startWith("You cannot use this service")
-    }
-
-    "display correct heading" in {
-      standardOrgDoc.body.getElementsByTag("h1").text mustBe "You cannot use this service"
-    }
-
-    "display para-1" in {
-      standardOrgDoc.body
-        .getElementById("para-1")
-        .text mustBe "You signed in to Government Gateway as a standard user. To apply for access to Advance Tariff Rulings you must be an administrator user."
-    }
-
-    "display para-2" in {
-      standardOrgDoc.body.getElementById(
-        "para-2"
-      ).text mustBe "Please log in as an administrator or contact the person who set up your Government Gateway."
-    }
-
     "have a Sign out button with the correct href" in {
       standardOrgDoc.body().getElementsByClass("govuk-button").attr("href") must endWith("/register/logout")
     }
   }
 
   "You cannot use this service page for users of type agent" should {
-
-    "display correct title" in {
-      agentDoc.title must startWith("You cannot use this service")
-    }
-
-    "display correct heading" in {
-      agentDoc.body.getElementsByTag("h1").text mustBe "You cannot use this service"
-    }
-
-    "display para-1" in {
-      agentDoc.body
-        .getElementById("para-1")
-        .text mustBe "You signed in to Government Gateway with an agent services account."
-    }
-
-    "display para-2" in {
-      agentDoc.body
-        .getElementById("para-2")
-        .text mustBe "You need to sign in with the Government Gateway for the organisation or individual that is applying for access to Advance Tariff Rulings."
-    }
-
     "have a Sign out button with the correct href" in {
       agentDoc.body().getElementsByClass("govuk-button").attr("href") must endWith("/register/logout")
     }
   }
 
-  "You cannot use this service page for users who cannot get an eori" should {
-
-    "display correct title" in {
-      cannotUseService003.title must startWith("You cannot use this service")
-    }
-
-    "display correct heading" in {
-      cannotUseService003.body.getElementsByTag("h1").text mustBe "You cannot use this service"
-    }
-
-    "display correct para" in {
-      cannotUseService003.body
-        .getElementById("para")
-        .text mustBe "To apply for an EORI number phone 0300 322 7067 and ask for an assisted digital application form. They’re open 8am to 6pm, Monday to Friday (except public holidays)."
-    }
-  }
-
   private lazy val standardOrgDoc: Document      = Jsoup.parse(contentAsString(youCantUseServiceView(Some(Organisation))))
   private lazy val agentDoc: Document            = Jsoup.parse(contentAsString(youCantUseServiceView(Some(Agent))))
-  private lazy val cannotUseService003: Document = Jsoup.parse(contentAsString(sub02View()))
 
 }
