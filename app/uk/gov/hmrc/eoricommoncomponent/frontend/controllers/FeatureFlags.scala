fc94d1e815722168c3e9f84466a7120b82684cc5
German_ <81871966+dinamit1984@users.noreply.github.com>
2023-06-02 08:33:39 +0100

    requested changes are done

--- app/uk/gov/hmrc/eoricommoncomponent/frontend/controllers/FeatureFlags.scala
+++ app/uk/gov/hmrc/eoricommoncomponent/frontend/controllers/FeatureFlags.scala
@@ -23,7 +23,6 @@ class FeatureFlags @Inject() (config: Configuration) {
 
   def loadConfig(path: String): Boolean = config.get[Boolean](path)
 
-  // hard-code flag in case the feature ever needs to be re-introduced
 
   val sub02UseServiceName: Boolean          = loadConfig("features.sub02UseServiceName")
   val arsNewJourney: Boolean                = loadConfig("features.arsNewJourney")
