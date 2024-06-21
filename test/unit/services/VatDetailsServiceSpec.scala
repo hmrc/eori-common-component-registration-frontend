1c4f1d54c1c8ad1bdb8b65f8ba4ec63048361c11
ashwinirudrappa <11343826+ashwinirudrappa@users.noreply.github.com>
2024-06-21 15:28:04 +0100

    DDCYLS-5442:format file

--- test/unit/services/VatDetailsServiceSpec.scala
+++ test/unit/services/VatDetailsServiceSpec.scala
@@ -24,7 +24,11 @@ import org.scalatest.BeforeAndAfterEach
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatestplus.mockito.MockitoSugar
 import uk.gov.hmrc.eoricommoncomponent.frontend.config.AppConfig
-import uk.gov.hmrc.eoricommoncomponent.frontend.connector.{GetVatCustomerInformationConnector, ResponseError, VatControlListConnector}
+import uk.gov.hmrc.eoricommoncomponent.frontend.connector.{
+  GetVatCustomerInformationConnector,
+  ResponseError,
+  VatControlListConnector
+}
 import uk.gov.hmrc.eoricommoncomponent.frontend.domain.{VatControlListRequest, VatControlListResponse}
 import uk.gov.hmrc.eoricommoncomponent.frontend.services.VatDetailsService
 import uk.gov.hmrc.http.HeaderCarrier
