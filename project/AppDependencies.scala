0d4c1c1fe5b978458d6f96fe2ce2522b042f25e6
Dips <13121210+dips7189@users.noreply.github.com>
2026-05-06 13:35:03 +0100

    DDCYLS-7872 (#584)
    * DDCYLS-7872 upgrading to newest play frontend giving the service navigation
    
    * DDCYLS-7872 upgrading dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,16 +4,16 @@ object AppDependencies {
   import play.core.PlayVersion
 
   val mongoDbVersion   = "2.12.0"
-  val bootstrapVersion = "10.5.0"
+  val bootstrapVersion = "10.7.0"
   val playVersion      = 30
 
   val compileDependencies: Seq[ModuleID] = Seq(
     "org.typelevel"     %% "cats-core"                                        % "2.10.0",
     "uk.gov.hmrc"       %% s"bootstrap-frontend-play-$playVersion"            % bootstrapVersion exclude("org.apache.commons", "commons-lang3"),
-    "uk.gov.hmrc"       %% s"play-conditional-form-mapping-play-$playVersion" % "3.4.0",
+    "uk.gov.hmrc"       %% s"play-conditional-form-mapping-play-$playVersion" % "3.5.0",
     "uk.gov.hmrc"       %% s"domain-play-$playVersion"                        % "11.0.0",
     "uk.gov.hmrc.mongo" %% s"hmrc-mongo-play-$playVersion"                    % mongoDbVersion,
-    "uk.gov.hmrc"       %% s"play-frontend-hmrc-play-$playVersion"            % "12.29.0",
+    "uk.gov.hmrc"       %% s"play-frontend-hmrc-play-$playVersion"            % "13.4.0",
     "uk.gov.hmrc"       %% s"internal-auth-client-play-$playVersion"          % "4.3.0",
     "ch.qos.logback"     % "logback-core"                                     % "1.5.21",
     "at.yawk.lz4"        %  "lz4-java"                                        % "1.10.3",
--- test/unit/views/partials/HeaderSpec.scala
+++ test/unit/views/partials/HeaderSpec.scala
@@ -72,7 +72,7 @@ class HeaderSpec extends ControllerSpec with AuthActionMock with CSRFTest {
       val result = controller.startRegister(atarService).apply(SessionBuilder.buildRequestWithSession(defaultUserId))
 
       val page = CdsPage(contentAsString(result))
-      page.elementIsPresent("//nav[@class='hmrc-language-select']") shouldBe true
+      page.elementIsPresent("//nav[@class='hmrc-service-navigation-language-select']") shouldBe true
     }
   }
 
